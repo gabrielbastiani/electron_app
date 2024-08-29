88a0b40db79d611bfb4054c9bc98aad979795369
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2024-08-29 17:06:04 -0300

    electron

--- src/renderer/src/pages/create.tsx
+++ src/renderer/src/pages/create.tsx
@@ -16,10 +16,66 @@ export function Create() {
     }
 
     return (
-        <div>
-            <h1>Página NOVO CLIENTE!!!</h1>
+        <div className="flex-1 flex flex-col py-12 px-10 gap-8 overflow-y-auto">
+            <section className="flex flex-1 flex-col items-center">
+                <h1 className="text-white text-xl lg:text-3xl font-semibold">
+                    Cadastrar novo cliente
+                </h1>
 
-            <button onClick={handleAddCustomer}>CADASTRAR</button>
+                <form className="w-full max-w-96 mt-4">
+                    <div className="mb-2">
+                        <label className="text-lg">Nome:</label>
+                        <input
+                            type="text"
+                            placeholder="Digite o nome do cliente..."
+                            className="w-full h-9 rounded text-black px-2"
+                        />
+                    </div>
+
+                    <div className="mb-2">
+                        <label className="text-lg">Endereço:</label>
+                        <input
+                            type="text"
+                            placeholder="Digite o endereço..."
+                            className="w-full h-9 rounded text-black px-2"
+                        />
+                    </div>
+
+                    <div className="mb-2">
+                        <label className="text-lg">Email:</label>
+                        <input
+                            type="text"
+                            placeholder="Digite o email do cliente..."
+                            className="w-full h-9 rounded text-black px-2"
+                        />
+                    </div>
+
+                    <div className="mb-2">
+                        <label className="text-lg">Telefone:</label>
+                        <input
+                            type="text"
+                            placeholder="Digite o telefone do cliente..."
+                            className="w-full h-9 rounded text-black px-2"
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="text-lg">Cargo:</label>
+                        <input
+                            type="text"
+                            placeholder="Digite o cargo do cliente..."
+                            className="w-full h-9 rounded text-black px-2"
+                        />
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="bg-blue-500 rounded flex items-center justify-center w-full h-9"
+                    >
+                        Cadastrar
+                    </button>
+                </form>
+            </section>
         </div>
     )
 }
